3412ab0d578ce244e1a203085e7f6c5a774f02af
DeliveryPerson2004 <xiaopeng.guan2004@gmail.com>
2026-08-20 17:07:56 +0800

    refactor: remove commented-out code from test.ts

--- src/test.ts
+++ src/test.ts
@@ -4,20 +4,3 @@ import {ResponsesSchema} from "./DeepSeek/API/responses/ResponsesSchema.ts";
 
 const modelClient = new ModelClient();
 console.log(typeof modelClient)
-
-// const input: InputType = "你好，你能帮我做什么？"
-//
-// const responses = await modelClient.requestResponsesAPI(
-//     "deepseek-v4-flash",
-//     input,
-//     "你是智能助手。",
-// )
-//
-// // const responsesParsed = ResponsesSchema.parse(responses);
-//
-// // console.log(responsesParsed);
-// for(const item of responses.output){
-//     if(item.type == "message" || item.type == "reasoning"){
-//         console.log(item.content);
-//     }
-// }
