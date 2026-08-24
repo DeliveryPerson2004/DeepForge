aa6425427b732931eb3d9aa4201f735a403a6aed
DeliveryPerson2004 <xiaopeng.guan2004@gmail.com>
2026-08-24 21:29:32 +0800

    chore: hardcode workspace path for testing

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import {Session} from "./Session.ts";
 
 
 async function main() {
-    const workspacePath = process.cwd();
+    const workspacePath = "/home/administrator/WebstormProjects/user-workspace";
 
     const session = await Session.createNewSession(workspacePath);
     await session.input("你是谁？你能帮我做什么？调用网络搜索工具查询天津市北辰区明日天气。");
