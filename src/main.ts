4bcccacbdac5df4ec31970b53ee28475330efd44
DeliveryPerson2004 <xiaopeng.guan2004@gmail.com>
2026-08-24 19:59:15 +0800

    feat: persist main function logs to database

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import {logger} from "./logger.ts";
+import {logAndInsertDataToDB, logger} from "./logger.ts";
 import {Session} from "./Session.ts";
 import {prisma} from "./database/prisma-client.ts";
 
 async function main() {
-    logger.info("function main() start");
-
     const workspacePath = process.cwd();
     const newSession = await prisma.session.create({
         data: {},
     });
+
+    await logAndInsertDataToDB("function main() start", "info", newSession.id);
     const session = new Session(workspacePath, newSession.id);
     await session.input("你是谁？你能帮我做什么？调用网络搜索工具查询天津市北辰区明日天气。");
 
