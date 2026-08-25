0ed34f3c14f18abb3d571625782f4ffddde4f929
DeliveryPerson2004 <xiaopeng.guan2004@gmail.com>
2026-08-25 15:11:20 +0800

    refactor: remove test session code from main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,11 +14,6 @@ async function main() {
         logger.info(session.max_turn);
         logger.info(session.workspace_path);
     }
-    // logger.info(allSession);
-
-    // const session = await Session.resumeSession(workspacePath, 1);
-    const session = await Session.createNewSession(workspacePath);
-    await session!.input("我最喜欢的水果是什么？？？我记得是西瓜？");
 }
 
 main().catch(console.error);
