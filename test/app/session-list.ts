7ded389e6c870b20e603ac3f95c2ced54dcecc19
DeliveryPerson2004 <xiaopeng.guan2004@gmail.com>
2026-08-25 20:57:25 +0800

    chore: remove obsolete endpoint test scripts

--- test/app/session-list.ts
+++
@@ -1,8 +0,0 @@
-import {logger} from "../../src/backend/logger.ts";
-
-const response = await fetch("http://localhost:30000/session-list", {
-    method: "GET",
-});
-
-const result = await response.json();
-logger.info(result);
