f79782679fa19facc4badba28256cc27f318cdef
DeliveryPerson2004 <xiaopeng.guan2004@gmail.com>
2026-08-25 15:10:58 +0800

    feat: add create and resume session methods to UserClient

--- src/UserClient.ts
+++ src/UserClient.ts
@@ -1,7 +1,17 @@
 import {prisma} from "./database/prisma-client.ts";
+import {Session} from "./Session.ts";
+
 
 export class UserClient {
     public async getSessionList(){
         return await prisma.session.findMany();
+    }
+
+    public async createNewSession(workspacePath: string){
+        return await Session.createNewSession(workspacePath);
+    }
+
+    public async resumeSession(workspacePath: string, sessionId: number){
+        return await Session.resumeSession(workspacePath, sessionId);
     }
 }
