1591b57fc90c89544aca5936143250b6e345a28c
DeliveryPerson2004 <xiaopeng.guan2004@gmail.com>
2026-08-25 20:28:16 +0800

    chore: add session-list and environment endpoints

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -1,12 +1,30 @@
 import express, { type Express, type Request, type Response } from 'express';
+import {UserClient} from "./UserClient.ts";
 
 const app: Express = express();
+const userClient = new UserClient();
 const port = 30000;
+
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!\n');
 });
 
+app.get('/session-list', async (req: Request, res: Response) => {
+    const sessionList = await userClient.getSessionList();
+
+    res.send(sessionList);
+});
+
+app.post('/environment', async (req: Request, res: Response) => {
+    const modelProvider: string = req.body.modelProvider;
+
+    const isReady = await userClient.checkEnvironment(modelProvider);
+
+    res.send(isReady);
+});
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`DeepForge server listening on port ${port}`);
 });
