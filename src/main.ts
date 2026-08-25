f296bf8076a22362267b9ea53876c793e6730368
DeliveryPerson2004 <xiaopeng.guan2004@gmail.com>
2026-08-25 17:09:05 +0800

    feat: add checkEnvironment and model connect validation to UserClient

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,11 @@
 import {UserClient} from "./UserClient.ts";
-import {logger} from "./logger.ts";
 
 
 
 async function main() {
     const workspacePath = "/home/administrator/WebstormProjects/user-workspace";
 
-    const client = new UserClient();
-    const allSession = await client.getSessionList();
-    for(const session of allSession){
-        logger.info(session.id);
-        logger.info(session.max_turn);
-        logger.info(session.workspace_path);
-    }
+    const client = new UserClient("deepseek");
 }
 
 main().catch(console.error);
