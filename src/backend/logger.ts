5199f124bf6d300675713bced6408242ab1eba09
DeliveryPerson2004 <xiaopeng.guan2004@gmail.com>
2026-08-27 10:19:12 +0800

    refactor: remove commented-out printLogAndSaveToDB from logger

--- src/backend/logger.ts
+++ src/backend/logger.ts
@@ -6,30 +6,6 @@ export type Log = {
     createdAt: Date,
 };
 
-// export async function printLogAndSaveToDB(
-//     content: string,
-//     logLevel: Level,
-//     sessionId: number,
-//     turn: number
-// ) {
-//     let contentLevel: ContentLevel = "info";
-//     if (logLevel === "info") {
-//         logger.info(content);
-//         contentLevel = ContentLevel.info;
-//     } else if (logLevel === "warn") {
-//         logger.warn(content);
-//         contentLevel = ContentLevel.warn;
-//     }
-//
-//     await prisma.log.create({
-//         data: {
-//             content: content,
-//             content_level: contentLevel,
-//             session_id: sessionId,
-//             turn: turn,
-//         }});
-// }
-
 export const logger = pino({
     transport: {
         target: 'pino-pretty',
