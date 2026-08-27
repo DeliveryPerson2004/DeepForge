a14f37dbbfe90fcde9cfb7be9f3d7c84c8681b73
DeliveryPerson2004 <xiaopeng.guan2004@gmail.com>
2026-08-27 11:55:55 +0800

    refactor: remove HTTP route handlers for sessions, environment, and model provider

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -1,6 +1,8 @@
 import express, { type Express, type Request, type Response } from 'express';
 import {AppServer} from "./AppServer.ts";
 
+
+
 const app: Express = express();
 const userClient = new AppServer();
 const port = 30000;
@@ -9,26 +11,6 @@ app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!\n');
-});
-
-app.get('/session-list', async (req: Request, res: Response) => {
-    const sessionList = await userClient.getSessionList();
-
-    res.send(sessionList);
-});
-
-app.get('/environment-is-ready', async (req: Request, res: Response) => {
-    const isReady = await userClient.checkEnvironment();
-
-    res.send(isReady);
-});
-
-app.post('/model-provider', async (req: Request, res: Response) => {
-    const modelProvider: string = req.body.modelProvider;
-
-    const isSuccess = userClient.setModelProvider(modelProvider);
-
-    res.send(isSuccess);
 });
 
 app.listen(port, () => {
