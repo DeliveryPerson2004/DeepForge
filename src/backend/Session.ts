611752ee49ed16ea2be40d6bccce22b80c96b2a3
DeliveryPerson2004 <xiaopeng.guan2004@gmail.com>
2026-08-27 10:19:29 +0800

    refactor: centralize session name generation and DB persistence in Session

--- src/backend/Session.ts
+++ src/backend/Session.ts
@@ -7,10 +7,10 @@ import {
     type ResponseSchema
 } from "./DeepSeek/API/responses.ts";
 import {PlanAgent} from "./DeepSeek/Agents/Planner/PlanAgent.ts";
-import {logger, type LogRecord} from "./logger.ts";
+import {logger, type Log} from "./logger.ts";
 import {prisma} from "./prisma-client.ts";
-import {ContentLevel} from "../../generated/prisma/enums.ts";
 import type {Level} from "pino";
+import {LogLevel} from "../../generated/prisma/enums.ts";
 
 
 
@@ -18,7 +18,7 @@ export class Session{
     private agent: BaseAgent;
     private readonly sessionId: number;
     private sessionName: string = "";
-    private logs: LogRecord[] = [];
+    private logs: Log[] = [];
 
     private constructor(baseAgent: BaseAgent, sessionId: number) {
         this.agent = baseAgent;
@@ -27,9 +27,115 @@ export class Session{
         this.printLogAndPushToLogs("new class Session()", "info");
     }
 
+    private getLogs(){
+        const agentLogs = this.agent.getLogs();
+        this.logs.push(...agentLogs);
+        const sessionLogs = this.logs;
+        this.logs = [];
+
+        return sessionLogs;
+    }
+
+    private async updateSessionName(userInput: string, agentInputAfterLoop: InputItemType[]){
+        const inputMessageItem: InputMessageItem = {
+            type: "message",
+            role: "user",
+            content: userInput,
+        }
+
+        agentInputAfterLoop.push(inputMessageItem)
+        const requestBody: RequestBody = {
+            model: ModelType.DeepSeekV4Flash,
+            input: agentInputAfterLoop,
+            instructions: "参考上述内容和格式化输出，生成会话名称",
+            user: "create_session_name",
+            text: {
+                format: {
+                    type: "json_schema",
+                    name: "session的名字",
+                    schema: {
+                        type: "object",
+                        properties: {
+                            sessionName: {
+                                type: "string",
+                                description: "生成的会话名称"
+                            }
+                        },
+                        required: ["sessionName"]
+                    }
+                }
+            }
+        }
+
+        const requestBodyString = JSON.stringify(requestBody);
+
+        const response = await fetch(
+            `https://api.deepseek.com/responses`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "Authorization": `Bearer ${process.env.DEEPSEEK_API_KEY}` // 此处传入实际的 API Token
+                },
+                body: requestBodyString,
+            }
+        );
+
+        const responseJSONed: ResponseSchema = await response.json();
+        for(const item of responseJSONed.output){
+            if(item.type == "message"){
+                for(const contentItem of item.content){
+                    const output = contentItem.text;
+                    const outputJSONed = JSON.parse(output);
+                    this.sessionName = outputJSONed.sessionName;
+                }
+            }
+        }
+
+        await prisma.session.update({
+            where: {id: this.sessionId},
+            data: {session_name: this.sessionName},
+        });
+    }
+
+    private async saveLogToDB(){
+        const logs = this.getLogs();
+
+        const logMapped = logs.map((log) => ({
+            session_id: this.sessionId,
+            content: log.content,
+            log_level: log.level as LogLevel,
+            created_at: log.createdAt,
+        }));
+
+        await prisma.log.createMany({
+            data: logMapped,
+        })
+    }
+
+    private async saveAgentInputToDB(agentInputBeforeLoop: InputItemType[], agentInputAfterLoop: InputItemType[]){
+        const inputDelta = agentInputAfterLoop.slice(agentInputBeforeLoop.length);
+
+        await prisma.agentInput.create({
+            data: {
+                input: inputDelta,
+                session_id: this.sessionId,
+                turn: this.agent.getTurn(),
+            }
+        })
+    }
+
+    private async updateAndSaveDataToDB(userInput: string, agentInputBeforeLoop: InputItemType[], agentInputAfterLoop: InputItemType[]) {
+        await this.updateSessionName(userInput, agentInputAfterLoop);
+        await this.saveLogToDB();
+        await this.saveAgentInputToDB(agentInputBeforeLoop, agentInputAfterLoop);
+    }
+
     private printLogAndPushToLogs(log: string, logLevel: Level){
-        const logRecord: LogRecord = {
+        const logRecord: Log = {
             content: log,
+            level: logLevel,
             createdAt: new Date(),
         }
 
@@ -50,22 +156,12 @@ export class Session{
         });
 
         for (const log of logs) {
-            if (log.content_level === ContentLevel.info) {
+            if (log.log_level === LogLevel.info) {
                 logger.info(log.content);
-            } else if (log.content_level === ContentLevel.warn) {
+            } else if (log.log_level === LogLevel.warn) {
                 logger.warn(log.content);
             }
         }
-    }
-
-    private async saveInputToDB(input: InputItemType[]){
-        await prisma.agentInput.create({
-            data: {
-                input: input,
-                session_id: this.sessionId,
-                turn: this.agent.getTurn(),
-            }
-        })
     }
 
     static async createSession(workspacePath: string) {
@@ -116,78 +212,15 @@ export class Session{
         return null;
     }
 
-    public async createSessionName(input: InputItemType[], newUserInput: string){
-        const inputMessageItem: InputMessageItem = {
-            type: "message",
-            role: "user",
-            content: newUserInput,
-        }
-
-        input.push(inputMessageItem)
-        const requestBody: RequestBody = {
-            model: ModelType.DeepSeekV4Flash,
-            input: input,
-            instructions: "参考上述内容和格式化输出，生成会话名称",
-            user: "create_session_name",
-            text: {
-                format: {
-                    type: "json_schema",
-                    name: "session的名字",
-                    schema: {
-                        type: "object",
-                        properties: {
-                            sessionName: {
-                                type: "string",
-                                description: "生成的会话名称"
-                            }
-                        },
-                        required: ["sessionName"]
-                    }
-                }
-            }
-        }
-
-        const requestBodyString = JSON.stringify(requestBody);
-
-        const response = await fetch(
-            `https://api.deepseek.com/responses`,
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                    "Authorization": `Bearer ${process.env.DEEPSEEK_API_KEY}` // 此处传入实际的 API Token
-                },
-                body: requestBodyString,
-            }
-        );
-
-        const responseJSONed: ResponseSchema = await response.json();
-        for(const item of responseJSONed.output){
-            if(item.type == "message"){
-                for(const contentItem of item.content){
-                    const output = contentItem.text;
-                    const outputJSONed = JSON.parse(output);
-                    this.sessionName = outputJSONed.sessionName;
-                }
-            }
-        }
-    }
-
     public async input(userInput: string) {
         this.printLogAndPushToLogs("class Session public input() start.", "info");
 
-        const agentInput = this.agent.getInput();
-        const turnStartInputLength = agentInput.length;
-        await this.createSessionName(agentInput, userInput);
+        const agentInputBeforeLoop = this.agent.getInput();
         await this.agent.loop(userInput);
-        const newAgentInput = this.agent.getInput();
-        await this.saveInputToDB(newAgentInput.slice(turnStartInputLength));
-        await prisma.session.update({
-            where: {id: this.sessionId},
-            data: {session_name: this.sessionName},
-        });
+        const agentInputAfterLoop = this.agent.getInput();
 
         this.printLogAndPushToLogs("class Session public input() end.", "info");
+
+        await this.updateAndSaveDataToDB(userInput, agentInputBeforeLoop, agentInputAfterLoop);
     }
 }
