55d6f988e5a03ba25a7dba6496a9b44aa1a04743
GXP <xiaopeng.guan2004@gmail.com>
2026-09-09 21:14:38 +0800

    refactor: replace Prisma ORM with native better-sqlite3

--- init-prisma.sh
+++
@@ -1,7 +0,0 @@
-# 检查两个路径是否都存在（-e 支持文件或目录），若同时存在则执行删除
-if [ -e "./generate" ] && [ -e "dev.db" ]; then
-  rm -rf ./generate dev.db ./prisma/migrations
-fi
-
-prisma migrate dev --name init
-prisma generate
