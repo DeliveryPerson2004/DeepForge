dc9d66a7258e913fe3ec54034f58e51d9127658b
GXP <xiaopeng.guan2004@gmail.com>
2026-09-10 21:36:44 +0800

    refactor: add select message prepared statement and type statements in stmt.ts

--- src/backend/database/stmt.ts
+++ src/backend/database/stmt.ts
@@ -1,4 +1,3 @@
-import "./initDatabase.ts";
 import { type Statement } from "better-sqlite3";
 import { db } from "./db.ts";
 
@@ -10,6 +9,10 @@ export const selectMaxTurnFromAgentTableStmt: Statement = db.prepare(`
     SELECT max_turn FROM agent WHERE id = ?;
 `).pluck();
 
-export const insertIntoMessageTableStmt: Statement = db.prepare(`
+export const insertIntoMessageTableStmt: Statement<[agentId: number, turn: number, content: string, isActivated: number]> = db.prepare(`
     INSERT INTO message (agent_id, turn, content, is_activated) VALUES (?, ?, ?, ?)
+`);
+
+export const selectMessageFromMessageTableStmt: Statement<[agentId: number], { content: string }> = db.prepare(`
+    SELECT content FROM message WHERE agent_id = ? AND is_activated = 1 ORDER BY created_at ASC, id ASC;
 `);
